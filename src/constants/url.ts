5f93249e97d2abeba914e1aa643a47c7a5234a28
ma9pie <ma9pie.dev@gmail.com>
2024-02-26 05:46:09 +0900

    feat : Footer 링크 추가

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,4 @@
 export const GITHUB_URL = 'https://github.com/ma9pie';
 export const BLOG_URL = 'https://cocoon1787.tistory.com';
+export const LINKEDIN_URL =
+  'https://www.linkedin.com/in/%EC%A4%80%EC%98%81-%EA%B9%80-5b9a19268/';
