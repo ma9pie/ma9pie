396ac1b2155a3a4dfce4f83165fdb79c11271203
ma9pie <ma9pie.dev@gmail.com>
2024-02-27 01:57:11 +0900

    feat: resume 페이지 Articles 추가

--- src/components/pages/resume/Articles.tsx
+++ src/components/pages/resume/Articles.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import tw, { styled } from 'twin.macro';
 
+import Text from '@/components/common/Text';
 import Container from '@/components/pages/resume/Container';
+import { ARTICLE_URLS } from '@/constants';
 
 const Articles = () => {
-  return <Container>Articles</Container>;
+  return (
+    <Container>
+      <Text.Title>Articles</Text.Title>
+
+      {ARTICLE_URLS.map(({ title, url }, idx) => (
+        <div key={idx}>
+          <Text.Link href={url} target="_blank">
+            {title}
+          </Text.Link>
+        </div>
+      ))}
+    </Container>
+  );
 };
 
 export default Articles;
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,3 +2,10 @@ export const GITHUB_URL = 'https://github.com/ma9pie';
 export const BLOG_URL = 'https://cocoon1787.tistory.com';
 export const LINKEDIN_URL =
   'https://www.linkedin.com/in/%EC%A4%80%EC%98%81-%EA%B9%80-5b9a19268/';
+export const ARTICLE_URLS = [
+  {
+    title:
+      '[React] 모달 깊게 파헤치기 : 효율적인 모달 관리에 대하여 (useModal custom hooks)',
+    url: 'https://cocoon1787.tistory.com/875',
+  },
+];
