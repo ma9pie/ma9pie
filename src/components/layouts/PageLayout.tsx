34caeffcfe36a962d964ece25bd1f779fd122674
ma9pie <ma9pie.dev@gmail.com>
2024-03-17 23:39:22 +0900

    design : PageLayout 조정

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -25,6 +25,6 @@ const Wrapper = styled.div`
 `;
 const Content = styled.div`
   ${tw`flex-1 flex flex-col max-w-[800px] w-full min-h-[calc(100vh-96px)] mx-auto`};
-  ${tw`mobile:(px-4 py-24)`};
-  ${tw`desktop:(px-6 py-40)`};
+  ${tw`mobile:(gap-4 px-4 py-24)`};
+  ${tw`desktop:(gap-6 px-6 py-40)`};
 `;
