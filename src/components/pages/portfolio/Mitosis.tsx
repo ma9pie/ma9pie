81b1a5ea9e88e992e3eb0596d6454711d58a4a44
ma9pie <ma9pie.dev@gmail.com>
2024-03-19 03:21:18 +0900

    add : Mitosis 추가

--- src/components/pages/portfolio/Mitosis.tsx
+++ src/components/pages/portfolio/Mitosis.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import tw, { styled } from 'twin.macro';
 
 import Img from '@/components/common/Img';
+import { Li, Ul } from '@/components/common/List';
+import Text from '@/components/common/Text';
+import Container from '@/components/pages/portfolio/Container';
 import Info from '@/components/pages/portfolio/Info';
 import { PORTFOLIO_PROJECTS } from '@/constants';
 import { Project } from '@/types';
@@ -10,15 +12,24 @@ const INFO = PORTFOLIO_PROJECTS[Project.Mitosis];
 
 const Mitosis = () => {
   return (
-    <Wrapper>
+    <Container>
       <Info {...INFO}></Info>
-      <Img src="/images/screenshots/mitosis/trade.png" width="100%"></Img>
-    </Wrapper>
+
+      <Img src="/images/screenshots/mitosis/trade.png" width="50%"></Img>
+
+      <Ul>
+        <Li>Tradingview chart Datafeed 구축</Li>
+        <Li>Tradingview chart customize option 추가</Li>
+      </Ul>
+
+      <Text.P>
+        타 부서에서 새로운 프로젝트에 tradingview chart 구축 관련하여 어려움을
+        겪고 있어 적극적으로 기술적 지원을 하였습니다. 베타 버전에서는 추세선만
+        그려주는 스펙으로 간단하게 Datafeed를 구축 및 설계를 하고, customizing
+        관련 코드에 상세한 주석을 추가하여 도움을 주었습니다.
+      </Text.P>
+    </Container>
   );
 };
 
 export default Mitosis;
-
-const Wrapper = styled.div`
-  ${tw``};
-`;
