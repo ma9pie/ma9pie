9d64055a3fe1b7aa07f3e3f375418b151b2f088a
ma9pie <ma9pie.dev@gmail.com>
2024-03-19 04:40:58 +0900

    refactor : PurpleBridge 리팩토링

--- src/components/pages/portfolio/PurpleBridge.tsx
+++ src/components/pages/portfolio/PurpleBridge.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import tw, { styled } from 'twin.macro';
 
+import Flex from '@/components/common/Flex';
+import Grid from '@/components/common/Grid';
 import Img from '@/components/common/Img';
+import { Li, Ul } from '@/components/common/List';
+import Spacing from '@/components/common/Spacing';
+import Text from '@/components/common/Text';
 import Info from '@/components/pages/portfolio/Info';
 import { PORTFOLIO_PROJECTS } from '@/constants';
 import { Project } from '@/types';
@@ -10,23 +15,34 @@ const INFO = PORTFOLIO_PROJECTS[Project.PurpleBridge];
 
 const PurpleBridge = () => {
   return (
-    <Wrapper>
+    <>
       <Info {...INFO}></Info>
-      <Img src="/images/screenshots/purplebridge/bridge.png" width="100%"></Img>
-      <Img src="/images/screenshots/purplebridge/swap.png" width="100%"></Img>
-      <Img
-        src="/images/screenshots/purplebridge/add-liquidity.png"
-        width="100%"
-      ></Img>
-      <Img
-        src="/images/screenshots/purplebridge/remove-liquidity.png"
-        width="100%"
-      ></Img>
+
+      <Spacing height={40}></Spacing>
+
+      <Grid col={2} gap={16}>
+        <Img
+          src="/images/screenshots/purplebridge/bridge.png"
+          width="100%"
+        ></Img>
+        <Img src="/images/screenshots/purplebridge/swap.png" width="100%"></Img>
+        <Img
+          src="/images/screenshots/purplebridge/add-liquidity.png"
+          width="100%"
+        ></Img>
+        <Img
+          src="/images/screenshots/purplebridge/remove-liquidity.png"
+          width="100%"
+        ></Img>
+      </Grid>
+
+      <Spacing height={16}></Spacing>
+
       <Img
         src="/images/screenshots/purplebridge/dashboard.png"
-        width="100%"
+        width="75%"
       ></Img>
-    </Wrapper>
+    </>
   );
 };
 
